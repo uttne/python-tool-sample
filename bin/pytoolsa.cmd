64f3e7415c1e824ce42226647a642ce1f05739d7
uttne <uttne.mkr@gmail.com>
2022-04-23 02:11:23 +0900

    python の実行ファイルを検索してコマンドを実行するように変更

--- bin/pytoolsa.cmd
+++ bin/pytoolsa.cmd
@@ -1,1 +1,27 @@
-@python -m python_tool_sample %*
+@echo off
+
+set command=
+for %%i in (py python python3) do (
+    for /f "usebackq delims=" %%j in (`where %%i 2^>^nul`) do (
+        call :func %%j
+    )
+)
+
+goto run
+
+:func
+if not exist "%command%" (
+    set command=%~1
+)
+
+goto :EOF
+
+:run
+
+if not exist "%command%" (
+    echo Python is not found.
+    exit /b
+)
+
+"%command%" -m python_tool_sample %*
+
